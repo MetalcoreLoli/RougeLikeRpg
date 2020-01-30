fbc2f563158709dd69cad913672b161d7a59c2cc
MetalcoreLoli <gogs@fake.local>
2020-01-30 19:01:02 +0300

    add test and attribute InternalsVisibleTo()

--- RougeLikeRpg.Game/Core/Vector2D.cs
+++ RougeLikeRpg.Game/Core/Vector2D.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Runtime.CompilerServices;
 
-
+[assembly: InternalsVisibleTo("RougeLikeRpg.Tests")]
 namespace RougeLikeRPG.Core
 {
+ 
     internal struct Vector2D
     {
         public Int32 X { get; set; }
@@ -23,9 +25,20 @@ namespace RougeLikeRPG.Core
 
         public static Vector2D operator +(Vector2D a, Int32 b)
             => new Vector2D(a.X + b, a.Y + b);
-        
+
         public static Vector2D operator -(Vector2D a, Int32 b)
-            => new Vector2D(a.X - b, a.Y - b);
-       
+           => new Vector2D(a.X - b, a.Y - b);
+
+        public static Int32 operator *(Vector2D a, Vector2D b)
+           => (b.X * a.X) + (b.Y * a.Y);
+
+        public static Vector2D operator *(Vector2D a, Int32 b)
+          => new Vector2D(a.X * b, a.Y * b);
+
+        public override bool Equals(object obj)
+        {
+            Vector2D b = (Vector2D)obj;
+            return this.X.Equals(b.X) && this.Y.Equals(b.Y);
+        }
     }
 }
--- RougeLikeRpg.Tests/UnitTest1.cs
+++ RougeLikeRpg.Tests/UnitTest1.cs
@@ -1,18 +1,29 @@
 using NUnit.Framework;
+using RougeLikeRPG.Core;
+using System;
 
 namespace RougeLikeRpg.Tests
 {
     public class Tests
     {
+        Vector2D vector;
         [SetUp]
         public void Setup()
         {
+            vector = new Vector2D(2, 2);
         }
 
         [Test]
-        public void Test1()
+        public void Vector2DScalarMulty()
         {
-            Assert.Pass();
+            Vector2D b = new Vector2D(3, 3);
+            Assert.AreEqual(vector * b, 12);
+        }
+
+        [Test]
+        public void Vector2DMultyOnScalar()
+        {
+            Assert.AreEqual(vector * 3, new Vector2D(6, 6));
         }
     }
 }
