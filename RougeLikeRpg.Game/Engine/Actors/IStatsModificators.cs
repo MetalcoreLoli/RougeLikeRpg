f954139bd5435c6fb43e68906bbff199d0aac862
MetalcoreLoli <deadpool1118@gmail.com>
2020-02-01 00:02:07 +0300

    Add IStatsModificators, HumanRace und HumanRaceTests

---
+++ RougeLikeRpg.Game/Engine/Actors/IStatsModificators.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace RougeLikeRPG.Engine.Actors
+{
+    internal interface IStatsModificator
+    {
+        /// <summary>
+        /// Сила
+        /// </summary>
+        Int32 StrMod { get; set; }
+        /// <summary>
+        /// Ловкость
+        /// </summary>
+        Int32 DexMod { get; set; }
+        /// <summary>
+        /// Интелект
+        /// </summary>
+        Int32 IntellMod { get; set; }
+        /// <summary>
+        /// Харизма
+        /// </summary>
+        Int32 ChariMod { get; set; }
+
+        /// <summary>
+        /// Удача
+        /// </summary>
+        Int32 LuckyMod { get; set; }
+
+    }
+}
---
+++ RougeLikeRpg.Tests/HumanRaceTest.cs
@@ -0,0 +1,24 @@
+using System;
+using NUnit.Framework;
+using RougeLikeRPG.Engine.Actors;
+using RougeLikeRPG.Engine.Actors.Races;
+
+namespace RougeLikeRpg.Tests
+{
+    [TestFixture]
+    public class HumanRaceTests
+    {
+        HumanRace race;
+        [SetUp]
+        public void Setup()
+        {
+            race = new HumanRace();
+        }
+
+        [Test]
+        public void Test()
+        {
+             
+        }
+    }
+}
