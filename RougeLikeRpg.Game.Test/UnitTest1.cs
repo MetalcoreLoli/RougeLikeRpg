f93d5ecc6ad89a08be5f88c681f5be5fc82b4a61
MetalcoreLoli <deadpool1118@gmail.com>
2021-03-03 20:27:36 +0300

    delete two old project und add new one for tests

---
+++ RougeLikeRpg.Game.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RougeLikeRpg.Game.Test
+{
+    public class EntityWorldSingletonTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
