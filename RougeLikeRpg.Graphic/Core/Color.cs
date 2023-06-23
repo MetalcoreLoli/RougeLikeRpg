fd8d1e039bc55001abca434aa714d1e48aa8d850
daniel fridman <deadpool1118@gmail.com>
2023-06-23 21:47:27 +0300

    changed colors into record struct

--- RougeLikeRpg.Graphic/Core/Color.cs
+++ RougeLikeRpg.Graphic/Core/Color.cs
@@ -3,29 +3,9 @@ using System;
 namespace RougeLikeRpg.Graphic.Core
 {
     [Serializable]
-    public struct Color
+    public readonly record struct Color(int Red, int Green, int Blue)
     {
-        public Int32 Red {get; set;}
-        public Int32 Green {get; set;}
-        public Int32 Blue {get; set;}
-
-        public Color(int red, int green, int blue)
-            => (Red, Green, Blue) = (red, green, blue);
-
-
-        public override bool Equals(object other)
-        {
-            if (other is Color otherColor)
-            {
-                return 
-                    Red   == otherColor.Red &&
-                    Green == otherColor.Green &&
-                    Blue  == otherColor.Blue;
-            }
-            else 
-                throw new Exception("Type Error");
-        }
-
         public override string ToString() => $"[{Red};{Green};{Blue}]";
+        public override int GetHashCode() => Red.GetHashCode() ^ Green.GetHashCode() ^ Blue.GetHashCode();
     }
 }
--- RougeLikeRpg.Graphic/Core/ColorManager.cs
+++ RougeLikeRpg.Graphic/Core/ColorManager.cs
@@ -1,24 +1,19 @@
-﻿using RougeLikeRpg.Graphic.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RougeLikeRpg.Graphic.Core
+﻿namespace RougeLikeRpg.Graphic.Core
 {
     public static class ColorManager
     {
-        public static Color White { get; }              = new Color(255, 255, 255);
-        public static Color Black { get; }              = new Color(0, 0, 0);
-        public static Color Green { get; }              = new Color(0, 255, 0);
-        public static Color Red   { get; }              = new Color(255, 0, 0);
+        public static Color White => new Color(255, 255, 255);
+        public static Color Black => new Color(0, 0, 0);
+        public static Color Green => new Color(0, 255, 0);
+        public static Color Red   => new Color(255, 0, 0);
 
-        public static Color DarkYellow { get; }         = new Color(107, 112, 0);
-        public static Color DarkGray { get; }           = new Color(128, 128, 128);
-        public static Color DarkRed { get; }            = new Color(52, 1, 1);
-        public static Color DarkBlue { get; }           = new Color(0, 0, 18);
-        public static Color DarkGreen { get; }          = new Color(0, 128, 0);
-        public static Color DarkMagenta { get; set; }   = new Color(56, 56, 56);
-        public static Color DarkCyan { get; set; }      = new Color(0, 128, 128);
-        public static Color Yellow { get; set; }        = new Color(242, 255, 0);
+        public static Color DarkYellow => new Color(107, 112, 0);
+        public static Color DarkGray   => new Color(128, 128, 128);
+        public static Color DarkRed    => new Color(52, 1, 1);
+        public static Color DarkBlue   => new Color(0, 0, 18);
+        public static Color DarkGreen  => new Color(0, 128, 0);
+        public static Color DarkMagenta => new Color(56, 56, 56);
+        public static Color DarkCyan => new Color(0, 128, 128);
+        public static Color Yellow => new Color(242, 255, 0);
     }
 }
